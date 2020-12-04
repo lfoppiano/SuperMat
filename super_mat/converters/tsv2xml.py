ee3088acb801ff2976a8201aa3328ccfaf625422
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2020-12-04 15:10:29 +0900

    add code

---
+++ super_mat/converters/tsv2xml.py
@@ -0,0 +1,377 @@
+# transform tei annotation into prodigy annotations
+import argparse
+import os
+from html import escape
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+
+
+def processFile(file):
+    output = {'paragraphs': [], 'rel_source_dest': [], 'rel_dest_source': []}
+
+    spans = []
+    tokens = []
+
+    currentParagraph = {'text': "", 'spans': spans, 'tokens': tokens, 'section': 'body'}
+
+    inside = False
+
+    with open(file) as fp:
+        tokenPreviousPositionEnd = '-1'
+        previousTagIndex = None
+        previousTagValue = None
+        tagIndex = None
+        tagValue = None
+        currentSpan = None
+        tokenId = 0
+        entitiesLayerFirstIndex = -1
+        sectionLayerFirstIndex = -1
+        hasDocumentStructure = False
+
+
+        # If there are no relationships, the TSV has two column less.
+        with_relationships = False
+        relation_source_dest = {}
+        relation_dest_source = {}
+        spans_layers = 3
+        relationship_layer_index = 5  # The usual value
+        for line in fp.readlines():
+            if line.startswith("#Text") and not inside:  # Start paragraph
+                currentParagraph['text'] = line.replace("#Text=", "")
+                inside = True
+                tokenId = 0
+
+            elif not line.strip() and inside:  # End paragraph
+                if currentSpan:
+                    spans.append(currentSpan)
+
+                output['paragraphs'].append(currentParagraph)
+                currentParagraph = {'text': "", 'spans': [], 'tokens': [], 'section': "body"}
+
+                spans = currentParagraph['spans']
+                tokens = currentParagraph['tokens']
+                tokenPreviousPositionEnd = '-1'
+                previousTagIndex = None
+                previousTagValue = None
+                tagIndex = None
+                tagValue = None
+                currentSpan = None
+
+                inside = False
+            else:
+                if not inside:
+                    if line.startswith("#T_SP"):
+                        layerName = line.split('|')[0].split('=')[1]
+                        if layerName == 'webanno.custom.Supercon':
+                            entitiesLayerFirstIndex = spans_layers
+                            entitiesLayerLabelIndex = entitiesLayerFirstIndex + 1
+                        elif layerName == 'webanno.custom.Section':
+                            sectionLayerFirstIndex = spans_layers
+                            hasDocumentStructure = True
+                        layerTagsets = len(line.split('|')) - 1
+                        spans_layers += layerTagsets
+
+                    if line.startswith("#T_RL"):
+                        with_relationships = True
+                        if spans_layers > 0:
+                            relationship_layer_index = entitiesLayerLabelIndex + 1
+
+                    print("Ignoring " + line)
+                    continue
+
+                split = line.split('\t')
+                annotationId = split[0]
+                position = split[1]
+                tokenPositionStart = position.split("-")[0]
+                tokenPositionEnd = position.split("-")[1]
+
+                if tokenPreviousPositionEnd != tokenPositionStart and tokenPreviousPositionEnd != '-1':  ## Add space in the middle #fingercrossed
+                    tokens.append(
+                        {'start': tokenPreviousPositionEnd, 'end': tokenPositionStart, 'text': " ", 'id': tokenId})
+                    tokenId = tokenId + 1
+
+                text = split[2]
+                tokens.append({'start': tokenPositionStart, 'end': tokenPositionEnd, 'text': text, 'id': tokenId})
+
+                section = "body"
+                if sectionLayerFirstIndex > -1:
+                    section = split[3].split('[')[0]
+
+                currentParagraph['section'] = section
+                tag = split[entitiesLayerLabelIndex].strip()
+                tag = tag.replace('\\', '')
+
+                if with_relationships:
+                    relationship_name = split[relationship_layer_index].strip()
+                    relationship_references = split[relationship_layer_index + 1].strip()
+                else:
+                    relationship_name = '_'
+                    relationship_references = '_'
+
+                relationships = []  # list of tuple(source, destination)
+
+                if relationship_name != '_' and relationship_references != '_':
+                    # We ignore the name of the relationship for the moment
+                    # names = relationship_name.split("|")
+
+                    # We split by | as they are grouped as
+                    # 2-162	1965-1969	YBCO	*[1]	material[1]	material-tc|material-tc	2-176[0_1]|2-179[0_1]
+                    references = relationship_references.split("|")
+                    for reference in references:
+                        reference_split = reference.split('[')
+                        if len(reference_split) == 1:
+                            # no disambiguation ids, so I use
+                            #   destination = layer-token (element 0)
+                            #   source = layer-token of reference (elemnt 6)
+
+                            source = reference
+                            destination = annotationId
+                        elif len(reference_split) > 1:
+                            reference_source_tsv = reference_split[0]
+                            source = reference_split[1].split('_')[0]
+                            destination = reference_split[1].split('_')[1][:-1]
+
+                            if source == '0':
+                                source = reference_source_tsv
+                            elif destination == '0':
+                                destination = annotationId
+
+                        relationships.append((source, destination))
+                        if source not in relation_source_dest:
+                            relation_source_dest[source] = [destination]
+                        else:
+                            relation_source_dest[source].append(destination)
+
+                        if destination not in relation_dest_source:
+                            relation_dest_source[destination] = [source]
+                        else:
+                            relation_dest_source[destination].append(source)
+
+                if tag != '_' and not tag.startswith('*'):
+                    if tag.endswith("]"):
+                        tagValue = tag.split('[')[0]
+                        tagIndex = tag.split('[')[1][:-1]
+                    else:
+                        tagValue = tag
+                        tagIndex = -1
+
+                    if tagIndex != -1:
+                        if tagIndex != previousTagIndex:
+                            if currentSpan:
+                                spans.append(currentSpan)
+                            currentSpan = {'start': tokenPositionStart, 'end': tokenPositionEnd, 'token_start': tokenId,
+                                           'token_end': tokenId, 'label': tagValue, 'tagIndex': tagIndex,
+                                           'relationships': relationships}
+                        else:
+                            if tagValue == previousTagValue:
+                                currentSpan['end'] = tokenPositionEnd
+                                currentSpan['token_end'] = tokenId
+                            else:
+                                if currentSpan:
+                                    spans.append(currentSpan)
+                                currentSpan = {'start': tokenPositionStart, 'end': tokenPositionEnd,
+                                               'token_start': tokenId,
+                                               'token_end': tokenId, 'label': tagValue, 'tagIndex': tagIndex,
+                                               'relationships': relationships}
+                    else:
+                        if currentSpan:
+                            spans.append(currentSpan)
+                        currentSpan = {'start': tokenPositionStart, 'end': tokenPositionEnd, 'token_start': tokenId,
+                                       'token_end': tokenId, 'label': tagValue, 'tagIndex': annotationId,
+                                       'relationships': relationships}
+
+                else:
+                    if currentSpan:
+                        spans.append(currentSpan)
+                        currentSpan = None
+
+                tokenId = tokenId + 1
+
+                tokenPreviousPositionEnd = tokenPositionEnd  # copy the position end
+                previousTagIndex = tagIndex  # index of the tag in the tsv
+                previousTagValue = tagValue
+
+        # print(output)
+        # if not line.startswith(str(paragraph_index) + "-"):
+        #     print("Something is wrong in the synchronisation " + str(paragraph_index) + " vs " + line[0:4])
+        #     sys.exit(-255)
+
+        # print(split)
+
+    output['paragraphs'].append(currentParagraph)
+    output['rel_dest_source'] = relation_dest_source
+    output['rel_source_dest'] = relation_source_dest
+    return output
+
+
+xmlTemplate = """<tei xmlns="http://www.tei-c.org/ns/1.0">
+    <teiHeader>
+        <fileDesc xml:id="_0">
+            <titleStmt/>
+            <publicationStmt>
+                <publisher>National Institute for Materials Science (NIMS), Tsukuba, Japan</publisher>
+                <availability>
+                    <licence target="http://creativecommons.org/licenses/by/3.0/">
+                        <p>The Creative Commons Attribution 3.0 Unported (CC BY 3.0) Licence applies to this document.</p>
+                    </licence>
+                </availability>
+            </publicationStmt>
+        </fileDesc>
+        <encodingDesc>
+            <appInfo>
+                <application version="project.version" ident="grobid-superconductors">
+                    <ref target="https://github.com/lfoppiano/grobid-superconductors">A machine learning software for extracting materials and their properties from scientific literature.</ref>
+                </application>
+            </appInfo>
+        </encodingDesc>
+        <profileDesc>
+            <abstract/>            
+        </profileDesc>
+    </teiHeader>
+    <text xml:lang="en">
+        <body/>
+    </text>
+</tei>"""
+
+
+def get_text_under_body(soup):
+    children = soup.findChildren('text')
+    return children[0] if children is not None and len(
+        children) > 0 else None
+
+
+def writeOutput(datas, output):
+    paragraphs = []
+    rel_dest_source = datas['rel_dest_source']
+    rel_source_dest = datas['rel_source_dest']
+    for data in datas['paragraphs']:
+        tokens = data['tokens']
+        spans = data['spans']
+        text = data['text']
+        section = data['section']
+        paragraph = ''
+
+        spanIdx = 0
+
+        for i, token in enumerate(tokens):
+            if spanIdx < len(spans):
+                span = spans[spanIdx]
+                span_token_start = span['token_start']
+                span_token_end = span['token_end']
+                span_label = span['label']
+            else:
+                span = None
+
+            if span is not None:
+                if i < span_token_start:
+                    paragraph += escape(token['text'])
+                    continue
+                    # paragraph += token['text']
+                elif span_token_start <= i <= span_token_end:
+                    if i == span_token_start:
+                        tagLabel = '<rs type="' + span_label + '">'
+                        pointers = ''
+                        identifier = ''
+                        if span['tagIndex'] in rel_source_dest:
+                            first = True
+                            for dest in rel_source_dest[span['tagIndex']]:
+                                if first:
+                                    first = False
+                                    pointers = ' corresp="#x' + dest
+                                else:
+                                    pointers += ',#x' + dest
+                            pointers += '"'
+
+                        if span['tagIndex'] in rel_dest_source:
+                            identifier = ' xml:id="x' + span['tagIndex'] + '"'
+
+                        if pointers is not '' or identifier is not '':
+                            tagLabel = '<rs type="' + span_label + '"' + identifier + pointers + '>'
+
+                        paragraph += tagLabel
+                    paragraph += escape(token['text'])
+                    if i == span_token_end:
+                        # paragraph += token['text']
+                        paragraph += '</rs>'
+                        spanIdx += 1
+
+            else:
+                paragraph += escape(token['text'])
+
+        paragraphs.append((section, paragraph))
+
+    with open(output, 'w') as fo:
+        soup = BeautifulSoup(xmlTemplate, 'xml')
+        for section, paragraphObj in paragraphs:
+            if section == 'title':
+                tag = BeautifulSoup('<title>' + paragraphObj + '</title>', 'xml')
+                soup.teiHeader.titleStmt.append(tag)
+            elif section == 'abstract':
+                tag = BeautifulSoup('<p>' + paragraphObj + '</p>', 'xml')
+                soup.teiHeader.profileDesc.abstract.append(tag)
+            elif section == 'keywords':
+                tag = BeautifulSoup('<ab type="keywords">' + paragraphObj + '</ab>', 'xml')
+                soup.teiHeader.profileDesc.append(tag)
+            elif section == 'body':
+                tag = BeautifulSoup('<p>' + paragraphObj + '</p>', 'xml')
+                text_tag = get_text_under_body(soup)
+                text_tag.body.append(tag)
+            elif section == 'figureCaption' or section == 'tableCaption':
+                tag = BeautifulSoup('<ab type="' + section + '">' + paragraphObj + '</ab>', 'xml')
+                text_tag = get_text_under_body(soup)
+                text_tag.body.append(tag)
+
+        fo.write(str(soup))
+        fo.flush()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Converter TSV to XML (Grobid training data based on TEI)")
+
+    parser.add_argument("--input", help="Input file or directory", required=True)
+    parser.add_argument("--output",
+                        help="Output directory (if omitted, the output will be the same directory/file with different extension)",
+                        required=False)
+    parser.add_argument("--recursive", action="store_true", default=False,
+                        help="Process input directory recursively. If input is a file, this parameter is ignored. ")
+
+    args = parser.parse_args()
+
+    input = args.input
+    output = args.output
+    recursive = args.recursive
+
+    if os.path.isdir(input):
+        path_list = []
+
+        if recursive:
+            for root, dirs, files in os.walk(input):
+                for file_ in files:
+                    if not file_.lower().endswith(".tsv"):
+                        continue
+
+                    abs_path = os.path.join(root, file_)
+                    path_list.append(abs_path)
+
+        else:
+            path_list = Path(input).glob('*.tsv')
+
+        for path in path_list:
+            print("Processing: ", path)
+            output_filename = Path(path).stem
+            data = processFile(path)
+            parent_dir = Path(path).parent
+            if os.path.isdir(str(output)):
+                output_path = os.path.join(output, str(output_filename)) + ".tei.xml"
+            else:
+                output_path = os.path.join(parent_dir, output_filename + ".tei.xml")
+
+            writeOutput(data, output_path)
+
+    elif os.path.isfile(input):
+        input_path = Path(input)
+        data = processFile(input_path)
+        output_filename = input_path.stem
+        writeOutput(data, os.path.join(output, str(output_filename) + ".tei.xml"))
---
+++ super_mat/converters/xml2tsv.py
@@ -0,0 +1,265 @@
+# transform XML Tei to TSV for WebAnno
+import argparse
+import os
+import re
+from pathlib import Path
+
+from bs4 import BeautifulSoup, NavigableString, Tag
+
+from grobid_tokenizer import tokenizeSimple
+
+
+def tokenise(string):
+    return tokenizeSimple(string)
+
+
+def write_on_file(fw, paragraphText, dic_token, i, item_length):
+    # tsvText += f'#Text={paragraphText}\n'
+    print(f'#Text={paragraphText}', file=fw)
+    for k, v in dic_token.items():
+        # print(v)
+        if k[0] == i + 1 and v[2]:
+            print('{}-{}\t{}-{}\t{}\t{}\t{}\t{}\t{}\t{}\t'.format(*k, *v), file=fw)
+
+    # Avoid adding a line break too much at the end of the file
+    if i != item_length - 1:
+        print('', file=fw)
+
+
+def getSection(pTag):
+    if pTag.name == 'p':
+        section = pTag.parent.name
+    elif pTag.name == 'ab':
+        if 'type' in pTag.attrs:
+            type = pTag.attrs['type']
+            if type == 'keywords':
+                section = "keywords"
+            elif type == 'figureCaption':
+                section = 'figureCaption'
+            elif type == 'tableCaption':
+                section = 'tableCaption'
+    elif pTag.name == 'title':
+        section = 'title'
+    else:
+        raise Exception("Something wrong")
+
+    return section
+
+
+def processFile(finput, foutput):
+    with open(finput, encoding='utf-8') as fp:
+        doc = fp.read()
+
+    mod_tags = re.finditer(r'(</\w+>) ', doc)
+    for mod in mod_tags:
+        doc = doc.replace(mod.group(), ' ' + mod.group(1))
+    #     print(doc)
+    soup = BeautifulSoup(doc, 'xml')
+
+    fw = open(foutput, 'w', encoding='utf-8')
+    print('#FORMAT=WebAnno TSV 3.2', file=fw)
+    print('#T_SP=webanno.custom.Section|name', file=fw)
+    print('#T_SP=webanno.custom.Supercon|extra_tag|supercon_tag', file=fw)
+    print('#T_RL=webanno.custom.Supercon_link|relationships|BT_webanno.custom.Supercon\n\n', file=fw)
+
+    children = []
+    for child in soup.tei.children:
+        if child.name == 'teiHeader':
+            children.append(child.find_all("title"))
+            children.extend([subchild.find_all("p") for subchild in child.find_all("abstract")])
+            children.append(child.find_all("ab", {"type": "keywords"}))
+        elif child.name == 'text':
+            children.append([subsubchild for subchild in child.find_all("body") for subsubchild in subchild.children if
+                             type(subsubchild) is Tag])
+
+    print(str(children))
+
+    off_token = 0
+    tsvText = ''
+    dic_token = {}
+    ient = 1
+
+    # list containing text and the dictionary with all the annotations
+    paragraphs = []
+    dic_dest_relationships = {}
+    dic_source_relationships = {}
+
+    i = 0
+    for child in children:
+        for pTag in child:
+            j = 0
+            section = getSection(pTag)
+            paragraphText = ''
+            for item in pTag.contents:
+                if type(item) == NavigableString:
+                    paragraphText += str(item)
+
+                    token_list = tokenise(item.string)
+                    if token_list[0] == ' ':  # remove space after tags
+                        del token_list[0]
+
+                    entity_class = '_'
+
+                    for token in token_list:
+                        s = off_token
+                        off_token += len(token.rstrip(' '))
+                        e = off_token
+                        if token.rstrip(' '):
+                            dic_token[(i + 1, j + 1)] = [
+                                s, e, token.rstrip(' '), section + f'[{i + 10000}]', entity_class, entity_class,
+                                entity_class, entity_class, entity_class]
+                            #                     print((i+1, j+1), s, e, [token], len(token.rstrip(' ')), off_token)
+                            j += 1
+                        if len(token) > 0 and token[-1] == ' ':
+                            off_token += 1  #
+                elif type(item) is Tag and item.name == 'rs':
+                    paragraphText += item.text
+
+                    token_list = tokenise(item.string)
+                    #                 token_list[-1] += ' ' # add space the end of tag contents
+                    if 'type' not in item.attrs:
+                        raise Exception("RS without type is invalid. Stopping")
+
+                    entity_class = item.attrs['type']
+                    link_name = '_'
+                    link_location = '_'
+
+                    if len(item.attrs) > 0:
+                        if 'xml:id' in item.attrs:
+                            if item.attrs['xml:id'] not in dic_dest_relationships:
+                                dic_dest_relationships[item.attrs['xml:id']] = [i + 1, j + 1, ient, entity_class]
+
+                        if 'corresp' in item.attrs:
+                            if (i + 1, j + 1) not in dic_source_relationships:
+                                dic_source_relationships[i + 1, j + 1] = [item.attrs['corresp'].replace('#', ''), ient,
+                                                                          entity_class]
+
+                            # link_to = dic_relationships[item.attrs['ptr'].replace("#", '')]
+                            # relationship_name = link_to[2] + '-' + entity
+                            # relationship_references = str(link_to[0]) + '-' + str(link_to[1]) + '[' + str(
+                            #     i + 1) + '-' + str(j + 1) + ']'
+                            # print(dic_token[link_to[0], link_to[1]])
+                        link_name = 'link_name'
+                        link_location = 'link_location'
+
+                    entity_class = entity_class.replace("_", "\_")
+
+                    for token in token_list:
+                        s = off_token
+                        off_token += len(token.rstrip(' '))
+                        e = off_token
+                        if token.rstrip(' '):
+                            dic_token[(i + 1, j + 1)] = [s, e, token.rstrip(' '), section + f'[{i + 10000}]',
+                                                         f'*[{ient}]',
+                                                         entity_class + f'[{ient}]', link_name, link_location]
+                            #                     print((i+1, j+1), s, e, [token], len(token.rstrip(' ')), off_token)
+                            j += 1
+                        if len(token) > 0 and token[-1] == ' ':
+                            off_token += 1  #
+                    ient += 1  # entity No.
+
+            off_token += 1  # return
+
+            paragraphs.append((i, paragraphText))
+            i += 1
+
+    for par_num, token_num in dic_source_relationships:
+        destination_xml_id = dic_source_relationships[par_num, token_num][0]
+        source_entity_id = dic_source_relationships[par_num, token_num][1]
+        label = dic_source_relationships[par_num, token_num][2]
+
+        # destination_xml_id: Use this to pick up information from dic_dest_relationship
+
+        for des in destination_xml_id.split(","):
+            destination_item = dic_dest_relationships[str(des)]
+            destination_paragraph_tsv = destination_item[0]
+            destination_token_tsv = destination_item[1]
+            destination_entity_id = destination_item[2]
+            destination_type = destination_item[3]
+
+            if str.lower(label) == 'tcvalue':
+                if destination_type == 'material':
+                    relationship_name = 'tcValue-material'
+                elif str.lower(destination_type) == 'me_method':
+                    relationship_name = 'tcValue-me_method'
+                else:
+                    raise Exception("Something is wrong in the links. "
+                                    "The origin label " + str(label) + ", or the destination " + str(
+                        destination_type) + " is not recognised. ")
+            elif str.lower(label) == 'pressure':
+                relationship_name = 'tcValue-pressure'
+            else:
+                raise Exception("Something is wrong in the links. "
+                                "The origin label " + str(label) + ", or the destination " + str(
+                    destination_type) + " is not recognised. ")
+
+            dict_coordinates = (destination_paragraph_tsv, destination_token_tsv)
+
+            dic_token_entry = dic_token[dict_coordinates]
+            if dic_token_entry[6] == 'link_name' and dic_token_entry[7] == 'link_location':
+                dic_token_entry[6] = relationship_name
+                dic_token_entry[7] = str(par_num) + '-' + str(token_num) + "[" + str(
+                    source_entity_id) + '_' + str(destination_entity_id) + ']'
+            else:
+                dic_token_entry[6] += '|' + relationship_name
+                dic_token_entry[7] += '|' + str(par_num) + '-' + str(token_num) + "[" + str(
+                    source_entity_id) + '_' + str(destination_entity_id) + ']'
+
+    # Cleaning up the dictionary token
+    for k, v in dic_token.items():
+        v[6] = v[6].replace('link_name', '_')
+        v[7] = v[7].replace('link_location', '_')
+
+    for paragraph in paragraphs:
+        write_on_file(fw, paragraph[1], dic_token, paragraph[0], len(paragraphs))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Converter XML (Grobid training data based on TEI) to TSV (Webanno )")
+
+    parser.add_argument("--input", help="Input file or directory", required=True)
+    parser.add_argument("--output", default=None,
+                        help="Output directory (if omitted, the output will be the same directory/file with different extension)")
+    parser.add_argument("--recursive", action="store_true", default=False,
+                        help="Process input directory recursively. If input is a file, this parameter is ignored. ")
+
+    args = parser.parse_args()
+
+    input = args.input
+    output = args.output
+    recursive = args.recursive
+
+    if os.path.isdir(input):
+        path_list = []
+
+        if recursive:
+            for root, dirs, files in os.walk(input):
+                for file_ in files:
+                    if not file_.lower().endswith(".tsv"):
+                        continue
+
+                    abs_path = os.path.join(root, file_)
+                    path_list.append(abs_path)
+
+        else:
+            path_list = Path(input).glob('*.tei.xml')
+
+        for path in path_list:
+            print("Processing: ", path)
+            output_filename = Path(path).stem
+            output_filename = output_filename.replace(".tei", "")
+            parent_dir = Path(path).parent
+
+            if os.path.isdir(str(output)):
+                output_path = os.path.join(output, str(output_filename) + ".tsv")
+            else:
+                output_path = os.path.join(parent_dir, output_filename + ".tsv")
+
+            processFile(str(path), output_path)
+
+    elif os.path.isfile(input):
+        input_path = Path(input)
+        data = processFile(input_path)
+        output_filename = input_path.stem
+        processFile(str(input_path), os.path.join(output, str(output_filename) + ".tsv"))
